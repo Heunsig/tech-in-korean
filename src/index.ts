f5b72e3e47326df948d5b12eb710f66af4ca92ab
Heunsig <heun3344@gmail.com>
2024-06-14 18:54:34 +0900

    feat: add static assets folder

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from 'express'
 const app = express()
 
 app.use(express.static('public'))
+app.use(express.static('assets'))
 
 // start express server and it should take PORT from environment variable
 app.listen(process.env.PORT || 3000, () => {
