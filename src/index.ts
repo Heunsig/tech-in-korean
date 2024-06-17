58c72ea850a93f1b88396410eb41cfc1dbcf1536
Heunsig <heun3344@gmail.com>
2024-06-17 22:10:46 +0900

    refactor: update express static asset routing

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.use(express.static('public'))
-app.use(express.static('assets'))
+app.use('/assets',express.static('assets'))
 
 // start express server and it should take PORT from environment variable
 app.listen(process.env.PORT || 3000, () => {
