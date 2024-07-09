0c6bc497253fd4da9f3ea4341c05767c77c996b7
Heunsig <heun3344@gmail.com>
2024-07-09 22:39:39 +0900

    chore: remove eleventy-navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,6 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.addPassthroughCopy("src/assets");
